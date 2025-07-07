02ddf24c899f5fc8220f437eb549f2727ceb0271
IT22079572 <it22079572@my.sliit.lk>
2025-07-07 13:35:34 +0530

    build  a model.py file

--- model/model.py
+++ model/model.py
@@ -1,21 +1,18 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd # type: ignore
+from sklearn.preprocessing import StandardScaler # type: ignore
+from sklearn.model_selection import train_test_split # type: ignore
+from sklearn.linear_model import LogisticRegression # type: ignore
+from sklearn.metrics import accuracy_score, classification_report # type: ignore
 import pickle as pickle
-
 
 
 def create_model(data): 
   X = data.drop(['diagnosis'], axis=1)
   y = data['diagnosis']
   
-  # scale the data
   scaler = StandardScaler()
   X = scaler.fit_transform(X)
   
-  # split the data
   X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
   )
@@ -28,15 +25,12 @@ def create_model(data):
   y_pred = model.predict(X_test)
   print('Accuracy of our model: ', accuracy_score(y_test, y_pred))
   print("Classification report: \n", classification_report(y_test, y_pred))
-  
   return model, scaler
 
 
 def get_clean_data():
   data = pd.read_csv("data/data.csv")
-  
   data = data.drop(['Unnamed: 32', 'id'], axis=1)
-  
   data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
   
   return data
